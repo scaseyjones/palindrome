d3e924674fc4e1899d77fd4ab0db16ac81e8be97
Casey Jones <scasey.jones@gmail.com>
2019-11-30 20:27:40 -0500

    Added npm package and readme

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-// Reverses a string.
-function reverse(string) {
+module.exports Phrase;
 
-    return Array.from(string).reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase object.
 function Phrase(content) {
-    this.content = content;
+  this.content = content;
 
-    this.processor = function(string) {
-        return string.toLowerCase();
-    }
-    
-    // Returns content processed for palindrome testing.
-    this.processedContent = function processedContent() {
-        return this.processor(this.content);
-    }
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
 
-    // Returns true if the phrase is a palindrome, false otherwise.
-    this.palindrome = function palindrome() {
-        return this.processedContent() === reverse(this.processedContent());
-    }
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
-
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-    // Returns translation processed for palindrome testing.
-    this.processedContent = function processedContent() {
-        return this.processor(this.translation);
-    }
-}
-
-TranslatedPhrase.prototype = new Phrase();
-
